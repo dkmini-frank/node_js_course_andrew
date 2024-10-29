147e6f32d2a3f36162d76532d43331164b3f18d6
frank.su <frank.su@au.harveynorman.com>
2024-10-29 16:22:27 +1100

    update

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,11 +1,11 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataJSON = dataBuffer.toString()
-const user = JSON.parse(dataJSON)
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const user = JSON.parse(dataJSON);
 
-user.name = 'Gunther'
-user.age = 54
+user.name = "Gunther";
+user.age = 54;
 
-const userJSON = JSON.stringify(user)
-fs.writeFileSync('1-json.json', userJSON)-
+const userJSON = JSON.stringify(user);
+fs.writeFileSync("1-json.json", userJSON);
